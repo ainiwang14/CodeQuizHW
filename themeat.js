cdc5b0b15d8fff845ea2250292842ae2d43a4a1f
AileenWang <ainiwang14@gmail.com>
2020-08-02 15:38:05 -0700

    -m

--- themeat.js
+++ themeat.js
@@ -2,11 +2,104 @@
 
 //write questions in an array called "myQuestions"
 
-//each question in array will be made of an object with question:; imgSrc:; choiceA:; etc; correct: "A"
+//each question in array will be made of an object with question:; (choice)A:; etc; correct: "A"
+
+var myQuestions = [
+    {
+        question: "Palmistry—the art of understanding an individual's present personality and predicting his or her future state through the study of the shape, size, and lines of the hands—is an unscientific technique. In a study, most palmists' assertions about the present financial status of the individuals in the sample population were found to be mere intelligent guesses. Which one of the following is an assumption necessary to the argument?",
+        A: "Individuals with the same financial status usually do not have a similar personality.",
+        B: "There is a stable correlation between an individual's personality and his or her financial status.",
+        C: "Palmistry is an effective means of predicting how personalities of individuals evolve over the long term.",
+        D: "There are numerous other methods for understanding the personality of an individual that are more precise than palmistry.",
+        E: "The financial future of a person is one of the most important concerns that palmists address.",
+        correct: "B"
+    }
+    {
+        question: "Each year, the number of students caught copying in examination is nearly the same as the number of students caught driving without a valid driving license and the number of students caught traveling without a valid ticket. Therefore, the outcry about copying in examination ought to be put to rest, as the act of copying in examination is in fact almost as mundane as the acts of driving without a valid driving license or traveling without a valid ticket. Which one of the following, if true, would most effectively undermine the author's argument?",
+        A: "Although the number of students caught driving without a valid driving license each year is very small, the total number of incidences of students traveling without a valid ticket is many times greater.",
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+    {
+        question:
+        A:
+        B:
+        C:
+        D:
+        E:
+        correct:
+    }
+]
 
 //event listeners for button clicks
 
-//create var timer = 500 i guess
+//create var timer = 500 i guess probably more tbh
 
 //timer should decrement every second
 
